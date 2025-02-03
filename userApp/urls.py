b1f24dbaeb046b6f62d88a82bd62e5f3022efed1
Sajin PJ <sajinsjn4433@gmail.com>
2025-02-03 22:27:05 +0530

    user registration completed

--- userApp/urls.py
+++ userApp/urls.py
@@ -2,11 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
     path('user_home',views.user_home,name='user_home'),
-
-
-
-
-
+    path('user_reg',views.user_reg,name='user_reg'),
 ]
--- userApp/views.py
+++ userApp/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
+from bridgeApp.models import *
+from django.http import *
 
 # Create your views here.
 def user_home(request):
     return render(request,'user_home.html')
+
+def user_reg(request):
+    if request.method=='POST':
+        name = request.POST['name']
+        email = request.POST['email']  
+        phone = request.POST['phone']
+        preferences = request.POST['preferences']  # Added preferences field
+        username = request.POST['username']  # Added username field
+        password = request.POST['password']  # Corrected 'pass' to 'password'
+
+
+        try:
+            lg=Login.objects.get(username=username,password=password)
+            if lg:
+                return HttpResponse("<script>alert('UserName Already Exist');window.location='/user_reg';</script>")
+        except:
+            lg=Login(username=username,password=password,usertype='user')
+            lg.save()
+
+            aud=Audience(name=name,email=email,phone=phone,login=lg)
+            aud.save()
+            return HttpResponse("<script>alert('User Added Successfully');window.location='/login';</script>")
+    return render(request,'user_reg.html')
